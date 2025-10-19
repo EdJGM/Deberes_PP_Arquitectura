d867be9e9297d2e79e2c9ea193ec69ae5390b920
Edgar <ejgallegos@espe.edu.ec>
2025-10-19 14:17:38 -0500

    feat: Agregar proyecto SOAP .NET y actualizar gitignore
    - Nuevo proyecto WCF/SOAP en .NET Framework 4.6
    - Agregar reglas de NetBeans al gitignore
    - Eliminar archivos privados de NetBeans del repositorio

---
+++ TI1.2 SOAP_DOTNET_SINBDD_GR06/ConUni_Soap_Dotnet_GR06/ConUni_Soap_Dotnet_GR06/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ConUni_Soap_Dotnet_GR06")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConUni_Soap_Dotnet_GR06")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// a los componentes COM. Si necesita obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e075bfd4-9cae-46be-8a55-cceef098c3e7")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados (número de compilación y de revisión) 
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
