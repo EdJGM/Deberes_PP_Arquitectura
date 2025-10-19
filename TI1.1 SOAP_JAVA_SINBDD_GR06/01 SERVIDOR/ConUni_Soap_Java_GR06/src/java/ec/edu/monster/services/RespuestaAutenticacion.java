91f8ee0226d94e2416615c54a65ad52bb730df4a
Edgar <ejgallegos@espe.edu.ec>
2025-10-19 12:41:40 -0500

    refactor(servicios): mover clase RespuestaAutenticacion al paquete 'services' y actualizar lógica

--- TI1.1 SOAP_JAVA_SINBDD_GR06/01 SERVIDOR/ConUni_Soap_Java_GR06/src/java/ec/edu/monster/services/RespuestaAutenticacion.java
+++ TI1.1 SOAP_JAVA_SINBDD_GR06/01 SERVIDOR/ConUni_Soap_Java_GR06/src/java/ec/edu/monster/services/RespuestaAutenticacion.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ec.edu.monster.modelo;
+package ec.edu.monster.services;
+
+import ec.edu.monster.modelo.Usuario;
 
 /**
  *
