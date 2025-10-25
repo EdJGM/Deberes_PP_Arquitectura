24d81b21f03b6b38cbeb08bd2d3924d239e8f431
Edgar <ejgallegos@espe.edu.ec>
2025-10-25 18:37:07 -0500

    Agrego implementación del servidor usando RESTful en .NET

---
+++ TI1.4 RESTFUL_DOTNET_SINBDD_GR06/ConUni_RestFul_Dotnet_GR06/ConUni_RestFul_Dotnet_GR06/Servicios/ConversorTemperatura.cs
@@ -0,0 +1,38 @@
+﻿namespace ConUni_RestFul_Dotnet_GR06.Servicios
+{
+    /// <summary>
+    /// Servicio de conversión de temperaturas
+    /// </summary>
+    public class ConversorTemperatura
+    {
+        public double CelsiusAFahrenheit(double celsius)
+        {
+            return (celsius * 9.0 / 5.0) + 32;
+        }
+
+        public double FahrenheitACelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5.0 / 9.0;
+        }
+
+        public double CelsiusAKelvin(double celsius)
+        {
+            return celsius + 273.15;
+        }
+
+        public double KelvinACelsius(double kelvin)
+        {
+            return kelvin - 273.15;
+        }
+
+        public double FahrenheitAKelvin(double fahrenheit)
+        {
+            return CelsiusAKelvin(FahrenheitACelsius(fahrenheit));
+        }
+
+        public double KelvinAFahrenheit(double kelvin)
+        {
+            return CelsiusAFahrenheit(KelvinACelsius(kelvin));
+        }
+    }
+}
