fa1dad58528249d71191546e3e94318350a1df1b
Edgar <ejgallegos@espe.edu.ec>
2025-10-25 18:40:21 -0500

    Reorganizo proyecto .NET: elimino carpeta antigua y agrego nueva ubicación

---
+++ TI1.4 RESTFUL_DOTNET_SINBDD_GR06/01 SERVIDOR/ConUni_RestFul_Dotnet_GR06/ConUni_RestFul_Dotnet_GR06/Servicios/ConversorMasa.cs
@@ -0,0 +1,41 @@
+﻿namespace ConUni_RestFul_Dotnet_GR06.Servicios
+{
+    /// <summary>
+    /// Servicio de conversión de masa
+    /// </summary>
+    public class ConversorMasa
+    {
+        // Kilogramo <-> Gramo
+        public double KilogramoAGramo(double kilogramos)
+        {
+            return kilogramos * 1000.0;
+        }
+
+        public double GramoAKilogramo(double gramos)
+        {
+            return gramos / 1000.0;
+        }
+
+        // Kilogramo <-> Libra
+        public double KilogramoALibra(double kilogramos)
+        {
+            return kilogramos * 2.20462;
+        }
+
+        public double LibraAKilogramo(double libras)
+        {
+            return libras / 2.20462;
+        }
+
+        // Gramo <-> Libra
+        public double GramoALibra(double gramos)
+        {
+            return KilogramoALibra(GramoAKilogramo(gramos));
+        }
+
+        public double LibraAGramo(double libras)
+        {
+            return KilogramoAGramo(LibraAKilogramo(libras));
+        }
+    }
+}
